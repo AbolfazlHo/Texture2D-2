17df6b4a1ebd96102e3a84807cf0c3ce33a64075
abolfazl <abolfazlhosnian@gmail.com>
2017-08-20 12:09:18 +0430

    2nd commit

---
+++ Assets/Scripts/Editor/EditorEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(EditorTest))]
+public class EditorEditor : Editor {
+	
+	public override void OnInspectorGUI ()
+	{
+		EditorTest b = (EditorTest)base.target;
+		b.myTexture = (Texture2D)EditorGUILayout.ObjectField ("Texture2D", b.myTexture, typeof(Texture2D), false);
+	}
+}
